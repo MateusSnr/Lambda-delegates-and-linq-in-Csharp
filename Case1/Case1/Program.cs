d06f6d477119816e7bda7504f8b7e64639bde6fe
“Mateus <devmateussnr@gmail.com>
2023-06-19 20:12:55 -0300

    Init push

---
+++ Case1/Case1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using Case1.Entities;
+using System.Linq;
+using System.IO;
+
+namespace Case1{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Product> productsList = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] fields = sr.ReadLine().Split(',');
+                    string name = fields[0];
+                    double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                    productsList.Add(new Product(name, price));
+                }
+            }
+            var avg = productsList.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var productsBelowAvg = productsList.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+            foreach (string p in productsBelowAvg)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
---
+++ Case2/Case2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using Case2.Entities;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Case2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employeesList = new List<Employee>();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] fileds = sr.ReadLine().Split(',');
+                        string name = fileds[0];
+                        string email = fileds[1];
+                        double salary = double.Parse(fileds[2], CultureInfo.InvariantCulture);
+                        employeesList.Add(new Employee(name, email, salary));
+                    }
+                }
+                Console.Write("Enter salary: ");
+                double salaryAux = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                var emails = employeesList.Where(p => p.Salary > salaryAux).OrderBy(p => p.Email).Select(p => p.Email);
+                foreach (string e in emails)
+                {
+                    Console.WriteLine(e);
+                }
+
+                var sum = employeesList.Where(obj => obj.Name[0] == 'M').Sum(obj => obj.Salary);
+
+                Console.WriteLine("Sum of salary of people whose name starts with 'M': " + sum.ToString("F2",CultureInfo.InvariantCulture));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error !");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
